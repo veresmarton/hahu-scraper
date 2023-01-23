c83dba33022819530b263222bf8e4c55f18ff37c
veresm <veresm3@gmail.com>
2023-01-23 22:38:04 +0100

    Run multiple queries

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ from bs4 import BeautifulSoup as bs
 def create_file(filename):
     if not os.path.exists(filename):
         colnames = ['run_datetime', 'query_name', 'make', 'model', 'link', 'price', 'fuel', 'year', 'engine_size', 'engine_power', 'mileage']
-        with open(filename, 'w') as file:
+        with open(filename, 'w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerow(colnames)
 
@@ -24,7 +24,8 @@ def scrape_pages(name, file, url):
     while next_page:
         print(page_number)
         page = requests.get(url + '/page'+str(page_number), verify=False)
-        assert page.status_code == 200, page.status_code
+        if page.status_code != 200:
+            break
         page_number += 1
         details = scrape_page(name, file, page)
 
@@ -36,6 +37,8 @@ def scrape_page(name, file, page):
             title = r.find("h3").find('a')
             link = title.get('href')
             make, model = title.text.split()[:2]
+            make = make.lower()
+            model = model.lower()
             
             price = r.select("div[class^=pricefield-primary]")[0].text.strip('Ft')
             price = ''.join(price.split())
@@ -54,15 +57,27 @@ def scrape_page(name, file, page):
             now = datetime.now()
 
             line = [now, name, make, model, link, price, fuel, year, engine_size, engine_power, mileage]
-            with open(file, 'a') as f:
+            with open(file, 'a', newline='', encoding='utf-8') as f:
                 writer = csv.writer(f)
                 writer.writerow(line)
 
+def run_queries(queryfile, dumpfile):
+    with open(queryfile, 'r', encoding='utf-8') as f:
+        qs = f.readlines()
+    for q in qs:
+        print(q)
+        name, url = q.split()
+        scrape_pages(name=name, file=dumpfile, url=url)
+
 if __name__ == "__main__":
     argParser = argparse.ArgumentParser()
+    argParser.add_argument("-q", "--queries", default=False)
     argParser.add_argument("-n", "--name")
     argParser.add_argument("-u", "--url")
     argParser.add_argument("-f", "--file", default='../data/raw/cars.csv')
     args = argParser.parse_args()
-    scrape_pages(name=args.name, file=args.file, url=args.url)
+    if args.queries:
+        run_queries(args.queries, args.file)
+    else:
+        scrape_pages(name=args.name, file=args.file, url=args.url)
 
