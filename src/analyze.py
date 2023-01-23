d4d8da100ad99d05e2625ac187217532469d7602
veresm <veresm3@gmail.com>
2023-01-23 22:39:20 +0100

    Analyze input arguments

--- src/analyze.py
+++ src/analyze.py
@@ -4,17 +4,18 @@ import argparse
 import pandas as pd
 import numpy as np
 
-def query_file(make, model, year_start, year_end, file):
+def query_file(make, model, year_start, file):
     df = pd.read_csv(file)
-    df = df.query('make==@make and model==@model and year<=@year_end and year>=year_start')
+    df = df.query('make==@make and model==@model and year<=@year+1 and year>=year-1')
     return df
 
 if __name__ == "__main__":
     argParser = argparse.ArgumentParser()
-    argParser.add_argument("-M", "--make")
-    argParser.add_argument("-m", "--model")
-    argParser.add_argument("-ys", "--year_start", default='2010')
-    argParser.add_argument("-ye", "--year_end", default='2020')
     argParser.add_argument("-f", "--file", default='../data/raw/cars.csv')
     args = argParser.parse_args()
-    df = query_file(args.make, args.model, args.year_start, args.year_end, args.file)
+    make = input('Make of car: ')
+    model = input('Model of car: ')
+    year = input('Year: ')
+    mileage = input('Mileage: ')
+    print(make, model, year, args.file)
+    df = query_file(make.lower(), model.lower(), int(year), args.file)
